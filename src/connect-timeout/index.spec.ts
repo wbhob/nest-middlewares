9c028543e2544554abb52afbbcc283c74ebef307
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-11-23 15:21:47 -0500

    feat(connect-timeout): add connect-timeout middleware

---
+++ src/connect-timeout/index.spec.ts
@@ -0,0 +1,34 @@
+import { ConnectTimeoutMiddleware } from './index';
+import { expect } from 'chai';
+
+describe('ConnectTimeoutMiddleware', () => {
+    let middleware: ConnectTimeoutMiddleware;
+    describe('properly configured', () => {
+        beforeEach(() => {
+            ConnectTimeoutMiddleware.configure('5s', {});
+            middleware = new ConnectTimeoutMiddleware();
+        });
+
+        it('should be defined', () => {
+            expect(middleware).to.not.be.undefined;
+        });
+
+        it('should have a function called resolve', () => {
+            expect(middleware.resolve).to.be.instanceof(Function);
+        });
+
+        it('should should return a middleware from calling resolve', () => {
+            expect(middleware.resolve()).to.be.an.instanceof(Function);
+        });
+        afterEach(() => {
+            ConnectTimeoutMiddleware.configure(undefined);
+        });
+    });
+
+    describe('not configured', () => {
+        middleware = new ConnectTimeoutMiddleware();
+        it('should should return a middleware from calling resolve', () => {
+            expect(middleware.resolve.bind(middleware)).to.throw(Error);
+        });
+    });
+});
---
+++ src/connect-timeout/index.ts
@@ -0,0 +1,25 @@
+import * as connectTimeout from 'connect-timeout';
+
+import { Middleware, NestMiddleware } from '@nestjs/common';
+
+import { RequestHandler } from 'express';
+
+@Middleware()
+export class ConnectTimeoutMiddleware implements NestMiddleware {
+
+    public static configure(timeout: string, opts?: connectTimeout.TimeoutOptions) {
+        this.timeout = timeout;
+        this.options = opts;
+    }
+
+    private static timeout: string;
+    private static options: connectTimeout.TimeoutOptions;
+
+    public resolve(...args: any[]): RequestHandler {
+        if (ConnectTimeoutMiddleware.options) {
+            return connectTimeout(ConnectTimeoutMiddleware.timeout, ConnectTimeoutMiddleware.options);
+        } else {
+            throw new Error('ConnectTimeoutMiddleware requires a timeout string in configure.');
+        }
+    }
+}
