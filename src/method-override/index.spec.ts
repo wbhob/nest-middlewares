eb42fb52218a170c43e39360d8da4a4ca699b27f
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-11-23 13:53:00 -0500

    feat(method-override): add method-override middleware

---
+++ src/method-override/index.spec.ts
@@ -0,0 +1,36 @@
+import { MethodOverrideMiddleware } from './index';
+import { expect } from 'chai';
+
+describe('MethodOverrideMiddleware', () => {
+    let middleware: MethodOverrideMiddleware;
+    describe('properly configured', () => {
+        beforeEach(() => {
+            MethodOverrideMiddleware.configure('X-HTTP-Method-Override', {
+                methods: ['POST'],
+            });
+            middleware = new MethodOverrideMiddleware();
+        });
+
+        it('should be defined', () => {
+            expect(middleware).to.not.be.undefined;
+        });
+
+        it('should have a function called resolve', () => {
+            expect(middleware.resolve).to.be.instanceof(Function);
+        });
+
+        it('should should return a middleware from calling resolve', () => {
+            expect(middleware.resolve()).to.be.an.instanceof(Function);
+        });
+        afterEach(() => {
+            MethodOverrideMiddleware.configure(undefined);
+        });
+    });
+
+    describe('not configured', () => {
+        middleware = new MethodOverrideMiddleware();
+        it('should should return a middleware from calling resolve', () => {
+            expect(middleware.resolve()).to.be.an.instanceof(Function);
+        });
+    });
+});
---
+++ src/method-override/index.ts
@@ -0,0 +1,28 @@
+import * as methodOverride from 'method-override';
+
+import { Middleware, NestMiddleware } from '@nestjs/common';
+import { Request, RequestHandler, Response } from 'express';
+
+@Middleware()
+export class MethodOverrideMiddleware implements NestMiddleware {
+
+    // DELETE THESE LINES IF MIDDLEWARE DOES NOT TAKE OPTIONS
+    public static configure(
+        getter: string | ((req: Request, res: Response) => string),
+        opts?: methodOverride.MethodOverrideOptions,
+    ) {
+        this.getter = getter;
+        this.options = opts;
+    }
+
+    private static options: methodOverride.MethodOverrideOptions;
+    private static getter: string | ((req: Request, res: Response) => string);
+
+    public resolve(...args: any[]): RequestHandler {
+        if (MethodOverrideMiddleware.options) {
+            return methodOverride(MethodOverrideMiddleware.getter, MethodOverrideMiddleware.options);
+        } else {
+            return methodOverride();
+        }
+    }
+}
