b0d4426793be08f628bb7d365f0c9877afa82a57
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-11-23 11:34:44 -0500

    feat(compression): add compression middleware

---
+++ src/compression/index.spec.ts
@@ -0,0 +1,40 @@
+import { CompressionMiddleware } from './index';
+import { expect } from 'chai';
+
+describe('CompressionMiddleware', () => {
+    let middleware: CompressionMiddleware;
+
+    describe('middleware configured', () => {
+        beforeEach(() => {
+            CompressionMiddleware.configure({
+                windowBits: 8,
+            });
+            middleware = new CompressionMiddleware();
+        });
+
+        it('should be defined', () => {
+            expect(middleware).to.not.be.undefined;
+        });
+
+        it('should have a function called resolve', () => {
+            expect(middleware.resolve).to.be.instanceof(Function);
+        });
+
+        it('should should return a middleware from calling resolve', () => {
+            expect(middleware.resolve()).to.be.an.instanceof(Function);
+        });
+        afterEach(() => {
+            CompressionMiddleware.configure(undefined);
+        });
+    });
+
+    describe('not configured', () => {
+        beforeEach(() => {
+            middleware = new CompressionMiddleware();
+        });
+
+        it('should throw an error for not being configured', () => {
+            expect(middleware.resolve()).to.be.an.instanceof(Function);
+        });
+    });
+});
---
+++ src/compression/index.ts
@@ -0,0 +1,24 @@
+import * as compression from 'compression';
+
+import { Middleware, NestMiddleware } from '@nestjs/common';
+
+import { RequestHandler } from 'express';
+
+@Middleware()
+export class CompressionMiddleware implements NestMiddleware {
+
+    public static configure(opts: compression.CompressionOptions) {
+        this.options = opts;
+    }
+
+    private static options: compression.CompressionOptions;
+
+
+    public resolve(...args: any[]) {
+        if (CompressionMiddleware.options) {
+            return compression(CompressionMiddleware.options);
+        } else {
+            return compression();
+        }
+    }
+}
