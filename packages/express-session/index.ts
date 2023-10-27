c8066512cebda6c4d70b68c8ae716c86ca7de5e6
Wilson Hobbs <wilsonhobbs1@gmail.com>
2023-10-27 02:18:44 -0700

    use singleton for expressSession

--- packages/express-session/index.ts
+++ packages/express-session/index.ts
@@ -1,21 +1,29 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import expressSession from 'express-session';
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import expressSession from "express-session";
 
 @Injectable()
 export class ExpressSessionMiddleware implements NestMiddleware {
+  // DELETE THESE LINES IF MIDDLEWARE DOES NOT TAKE OPTIONS
+  public static configure(opts: expressSession.SessionOptions) {
+    this.options = opts;
+  }
 
-    // DELETE THESE LINES IF MIDDLEWARE DOES NOT TAKE OPTIONS
-    public static configure(opts: expressSession.SessionOptions) {
-        this.options = opts;
+  private static options: expressSession.SessionOptions;
+  private singleton: ReturnType<typeof expressSession>;
+
+  private getSingleton() {
+    if (!this.singleton) {
+      if (ExpressSessionMiddleware.options) {
+        this.singleton = expressSession(ExpressSessionMiddleware.options);
+      } else {
+        this.singleton = expressSession();
+      }
     }
 
-    private static options: expressSession.SessionOptions;
+    return this.singleton;
+  }
 
-    public use(req: any, res: any, next: any) {
-        if (ExpressSessionMiddleware.options) {
-            expressSession(ExpressSessionMiddleware.options)(req, res, next);
-        } else {
-            expressSession()(req, res, next);
-        }
-    }
+  public use(req: any, res: any, next: any) {
+    this.getSingleton()(req, res, next);
+  }
 }
